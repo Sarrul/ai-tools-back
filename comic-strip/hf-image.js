218fcd1125764b8105b7b2cb7e7369324cbcaa8a
25LP5320 25LP5320 <25LP5320@G302-CC0093.local>
2025-12-24 12:10:43 +0800

    finallll

--- comic-strip/hf-image.js
+++ comic-strip/hf-image.js
@@ -13,17 +13,14 @@ simple background, cartoon style, 2D illustration
 ${prompt}
 `;
 
-  const output = await replicate.run(
-    "prompthero/openjourney-v4:e8818682e72a8b25895c7d90e889b712b6edfc5151f145e3606f21c1e85c65bf",
-    {
-      input: {
-        prompt: styledPrompt,
-        width: 1024,
-        height: 1024,
-        num_outputs: 1,
-      },
-    }
-  );
+  const output = await replicate.run("google/imagen-4", {
+    input: {
+      prompt: styledPrompt,
+      width: 1024,
+      height: 1024,
+      num_outputs: 1,
+    },
+  });
 
   // Replicate returns image URLs
   return output[0];
