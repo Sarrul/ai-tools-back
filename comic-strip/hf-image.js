3df02806b543af82d12cc9eef549628e9a502f68
25LP5320 25LP5320 <25LP5320@G302-CC0093.local>
2025-12-24 11:34:46 +0800

    real final

--- comic-strip/hf-image.js
+++ comic-strip/hf-image.js
@@ -14,7 +14,7 @@ ${prompt}
 `;
 
   const output = await replicate.run(
-    "stability-ai/stable-diffusion-xl-base-1.0",
+    "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
     {
       input: {
         prompt: styledPrompt,
